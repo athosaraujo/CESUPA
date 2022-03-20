9c2db286972acbc802d7b169661d99c76398fadb
Athos Andrade Silva de Araujo <100285947+athosaraujo@users.noreply.github.com>
2022-03-20 11:13:47 -0300

    Update and rename 03.c to 03.progGeometrica.c

--- Programação 1/Atividades/25 02 22/03.progGeometrica.c
+++ Programação 1/Atividades/25 02 22/03.progGeometrica.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-int pg(int a, int b, int c){
-	int i;
-	for(i=0;i<c;++i){
-	a = a * b;
-}
-	return a;
-	}
 int main(){
-	int a, b, c;
+	int a, b, c, i;
 	printf("por favor insira o numero inicial: ");
 	scanf("%d", &a);
 	printf("por favor insira a razao: ");
 	scanf("%d", &b);
 	printf("por favor insira a posicao desejada: ");
 	scanf("%d", &c);
-	printf("O %do valor da progressao geometrica eh %d\n",c, pg(a, b, c));
+	for(i=0;i<c-1;++i){a = a * b;}
+	printf("O %do valor da progressao geometrica eh %d\n",c, a);
 }
