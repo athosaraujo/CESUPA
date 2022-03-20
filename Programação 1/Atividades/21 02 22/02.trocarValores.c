503a4d03b927513476620350b69c5fc7b57c4387
Athos Andrade Silva de Araujo <100285947+athosaraujo@users.noreply.github.com>
2022-03-20 10:53:21 -0300

    Update and rename 02.c to 02.trocarValores.c

--- Programação 1/Atividades/21 02 22/02.trocarValores.c
+++ Programação 1/Atividades/21 02 22/02.trocarValores.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 int main(){
-	int a, b, c;
+	int a, b,c;
 	printf("Insira o valor de a: ");
 	scanf("%d", &a);
 	printf("Insira o valor de b: ");
 	scanf("%d", &b);
 	printf("valor de a e b sao: %d %d\n", a, b);
-	
-	c=a;
-	a=b;
-	b=c;
-	
+	a -= b;	b += a;	a = b - a;
 	printf("Depois da troca os valores de a e b sao: %d %d\n", a, b);
 }
