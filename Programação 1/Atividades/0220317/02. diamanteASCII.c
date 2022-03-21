6d029ac70d957d184dc540015af11c6ac95f8148
Athos Andrade Silva de Araujo <100285947+athosaraujo@users.noreply.github.com>
2022-03-21 09:50:24 -0300

    Tweaked Code

--- Programação 1/Atividades/0220317/02. diamanteASCII.c
+++ Programação 1/Atividades/0220317/02. diamanteASCII.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
   int n, i, j, inicio;
   inicio:
   printf("Insira a altura do diamante (numeros impares apenas): ");
@@ -9,13 +10,13 @@ int main(){
   n = (n+1)/2;
 
 /*----------metade de cima----------*/
-  for (i = 1; i <= n; i++){
-    for (j = 1; j <= n-i; j++)printf(" ");
-    for (j = 1; j <= 2*i-1; j++)printf("*");
-    printf("\n");}
+	for (i = 1; i <= n; i++){
+		for (j = 1; j <= n-i; j++)printf(" ");
+		for (j = 1; j <= 2*i-1; j++)printf("*");
+		printf("\n");}
 /*----------metade de baixo---------*/
 	for (i = 1; i <= n - 1; i++){
-		for (j = 1; j <= i; j++)
-		printf(" ");
-		for (j = 1 ; j <= 2*(n-i)-1; j++)
-		printf("*");printf("\n");}}
+		for (j = 1; j <= i; j++){printf(" ");}
+		for (j = 1 ; j <= 2*(n-i)-1; j++){printf("*");}
+		printf("\n");}
+}
