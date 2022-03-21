7d0457872067a47895176bf364424b353d0ea6c9
Athos Andrade Silva de Araujo <100285947+athosaraujo@users.noreply.github.com>
2022-03-21 09:35:05 -0300

    Fixed code

--- Programação 1/Atividades/0220317/02. diamanteASCII.c
+++ Programação 1/Atividades/0220317/02. diamanteASCII.c
@@ -2,19 +2,22 @@
 
 int main()
 {
-  int n, i, j;
-
-  printf("Insira a altura de metade do diamante: ");
+  int n, i, j, inicio;
+  inicio:
+  printf("Insira a altura do diamante (numeros impares apenas): ");
   scanf("%d", &n);
+  if(n %2 == 0 ){printf("insira um valor valido\n"); goto inicio;}
+  n = (n+1)/2;
+
 /*----------metade de cima----------*/
   for (i = 1; i <= n; i++){
-    for (j = 1; j <= n-i; j++){printf(" ");}
-    for (j = 1; j <= 2*i-1; j++){printf("*");}
+    for (j = 1; j <= n-i; j++)printf(" ");
+    for (j = 1; j <= 2*i-1; j++)printf("*");
     printf("\n");}
 /*----------metade de baixo---------*/
 	for (i = 1; i <= n - 1; i++){
-		for (j = 1; j <= i; j++){
-		printf(" ");}
-		for (j = 1 ; j <= 2*(n-i)-1; j++){
-		printf("*");printf("\n");}}
+		for (j = 1; j <= i; j++)
+		printf(" ");
+		for (j = 1 ; j <= 2*(n-i)-1; j++)
+		printf("*");printf("\n");}
 }
