2f57e7c792e0cdc4ce45fa5adf9bc06102d40bb3
Athos Andrade Silva de Araujo <100285947+athosaraujo@users.noreply.github.com>
2022-03-21 10:03:18 -0300

    Added even numbers

--- Programação 1/Atividades/0220317/02. diamanteASCII.c
+++ Programação 1/Atividades/0220317/02. diamanteASCII.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-  int n, i, j, inicio;
+  int n,num, i, j, inicio;
   inicio:
   printf("Insira a altura do diamante (numeros impares apenas): ");
   scanf("%d", &n);
-  if(n %2 == 0 ){printf("insira um valor valido\n"); goto inicio;}
+  num = n;
   n = (n+1)/2;
 
 /*----------metade de cima----------*/
@@ -14,9 +14,16 @@ int main()
 		for (j = 1; j <= n-i; j++){printf(" ");}
 		for (j = 1; j <= 2*i-1; j++){printf("*");}
 		printf("\n");}
+if(num%2==0){
+/*----------metade de baixo---------*/
+	for (i = 0; i <= n - 1; i++){
+		for (j = 1; j <= i; j++){printf(" ");}
+		for (j = 1 ; j <= 2*(n-i)-1; j++){printf("*");}
+		printf("\n");}
+}else{
 /*----------metade de baixo---------*/
 	for (i = 1; i <= n - 1; i++){
 		for (j = 1; j <= i; j++){printf(" ");}
 		for (j = 1 ; j <= 2*(n-i)-1; j++){printf("*");}
-		printf("\n");}
-}
+		printf("\n");}}}
+
